dbdaef8a6c01b8b391d2fc3a9a8e62556ba3ebe5
Adam Pond <adam.pond@hotmail.co.uk>
2020-02-09 10:04:48 +0000

    added password regex & error message, button added disabled function working correctly if email and password valid

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -7,14 +7,18 @@
 	let typingTimer;                //timer identifier
 	let finishedTypingInterval = 3000;  //time in ms (3 seconds)
     let emailInputRef;	// for .focus() onMount and if validation fails
+    let passwordInputRef;
     let emailError;
-    
+    let passwordError;
+    let valid = true;
     
     const emailRegex = RegExp( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/ )     // email regex test
+    const passwordRegex = RegExp( /^.{9,}$/ )
 
     // remove error message if field empty
-    $: if (!email) {
+    $: if (!email || !password) {
         emailError = '';
+        passwordError = '';
     }
 
 
@@ -25,9 +29,12 @@
 
 	const typeTimer = () => {
 		clearTimeout(typingTimer); 	//reset the timer on keyup
-		if (email) {		// if email exists
+		if (email && !password) {		// if email exists
 			typingTimer = setTimeout(() => checkEmail(email), finishedTypingInterval);	// run the 'checkEmail' function after the 'finishedTypingInterval' amount of time
-		}
+        }
+        if (password) {
+            typingTimer = setTimeout(() => checkPassword(password), finishedTypingInterval);	// run the 'checkEmail' function after the 'finishedTypingInterval' amount of time
+        }
 
 	}
 	const checkEmail = (email) => {
@@ -39,6 +46,19 @@
         } else {
             console.log('email passed')
             emailError = ''
+        }
+        
+	}
+	const checkPassword = (password) => {
+        console.log('checking password')
+	    if (!passwordRegex.test(password)) {
+            console.log('password failed')
+            passwordInputRef.focus() 	// refocus on email input if validation failed
+            passwordError = 'Please enter a valid password - must be more than 8 characters'
+        } else {
+            console.log('password passed')
+            passwordError = ''
+            valid = false;
         }
         
 	}
@@ -54,7 +74,12 @@
             <div>{emailError}</div>
         {/if}
 
-		<input autocomplete="off" bind:value={password} placeholder="enter password" type='password'>
-	
+		<input bind:this={passwordInputRef} autocomplete="off"  on:focusout={() => checkPassword(password)} on:keyup={typeTimer} bind:value={password} placeholder="enter password" type='password'>
+         {#if passwordError}
+            <div>{passwordError}</div>
+        {/if}
+
+        <button on:click|preventDefault={() => console.log('login successful')} disabled={valid}>Login</button>
+
 	
 	</form>
