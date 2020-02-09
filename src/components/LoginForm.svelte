36013694f3a5fa58ac72b781d17a688eac54ee17
Adam Pond <adam.pond@hotmail.co.uk>
2020-02-09 09:39:46 +0000

    email regex check added, error message displaying

--- src/components/LoginForm.svelte
+++ src/components/LoginForm.svelte
@@ -2,12 +2,20 @@
     import { onMount } from 'svelte';
   
 
-    let name;
+    let email;
 	let password;
 	let typingTimer;                //timer identifier
 	let finishedTypingInterval = 3000;  //time in ms (3 seconds)
-	let emailInputRef;	// for .focus() onMount and if validation fails
+    let emailInputRef;	// for .focus() onMount and if validation fails
+    let emailError;
+    
+    
+    const emailRegex = RegExp( /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/ )     // email regex test
 
+    // remove error message if field empty
+    $: if (!email) {
+        emailError = '';
+    }
 
 
 	onMount(() => {
@@ -17,14 +25,22 @@
 
 	const typeTimer = () => {
 		clearTimeout(typingTimer); 	//reset the timer on keyup
-		if (name) {		// if name exists
-			typingTimer = setTimeout(checkEmail, finishedTypingInterval);	// run the 'checkEmail' function after the 'finishedTypingInterval' amount of time
+		if (email) {		// if email exists
+			typingTimer = setTimeout(() => checkEmail(email), finishedTypingInterval);	// run the 'checkEmail' function after the 'finishedTypingInterval' amount of time
 		}
 
 	}
-	const checkEmail = () => {
-		console.log('checkEmail')
-		emailInputRef.focus() 	// refocus on email input if validation failed
+	const checkEmail = (email) => {
+        console.log('checking email')
+	    if (!emailRegex.test(email)) {
+            console.log('email failed')
+            emailInputRef.focus() 	// refocus on email input if validation failed
+            emailError = 'Please enter a valid email address'
+        } else {
+            console.log('email passed')
+            emailError = ''
+        }
+        
 	}
 
 </script>
@@ -33,7 +49,10 @@
 
 	<form>
 	
-		<input bind:this={emailInputRef} on:blur={checkEmail} on:keyup={typeTimer} bind:value={name} placeholder="enter email" type='email'>
+		<input bind:this={emailInputRef} on:blur={() => checkEmail(email)} on:keyup={typeTimer} bind:value={email} placeholder="enter email" type='email'>
+        {#if emailError}
+            <div>{emailError}</div>
+        {/if}
 
 		<input autocomplete="off" bind:value={password} placeholder="enter password" type='password'>
 	
