8ab27ad324dfe04890e206d343a0fda98948277b
Adam Pond <adam.pond@hotmail.co.uk>
2020-02-09 09:09:46 +0000

    form fields added, logic added for typetimer, email check, focus onmount

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,53 @@
 <script>
-	export let name;
+	import { onMount } from 'svelte';
+	let name;
+	let password;
+	let typingTimer;                //timer identifier
+	let finishedTypingInterval = 3000;  //time in ms (3 seconds)
+	let emailInputRef;	// for .focus() onMount and if validation fails
+
+
+
+	onMount(() => {
+        emailInputRef.focus()	// focus email input on page load
+    });
+
+
+	const typeTimer = () => {
+		clearTimeout(typingTimer); 	//reset the timer on keyup
+		if (name) {		// if name exists
+			typingTimer = setTimeout(checkEmail, finishedTypingInterval);	// run the 'checkEmail' function after the 'finishedTypingInterval' amount of time
+		}
+
+	}
+	const checkEmail = () => {
+		console.log('checkEmail')
+		emailInputRef.focus() 	// refocus on email input if validation failed
+	}
+
 </script>
 
+
+
+
+
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1>Form Test</h1>
+	<form>
+	
+		<input bind:this={emailInputRef} on:blur={checkEmail} on:keyup={typeTimer} bind:value={name} placeholder="enter email" type='email'>
+
+		<input autocomplete="off" bind:value={password} placeholder="enter password" type='password'>
+	
+	
+	</form>
 </main>
 
+
+
+
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
